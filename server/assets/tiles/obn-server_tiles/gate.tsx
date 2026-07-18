87ea3304c3b2ec68fcdb70ee8269d3599207d230
storymasterq <storymasterq@gmail.com>
2026-07-18 13:02:35 +0800

    Added tomb Updated gate Updated square with new floor
    Initial should be done. Now for the actual development.

--- server/assets/tiles/obn-server_tiles/gate.tsx
+++ server/assets/tiles/obn-server_tiles/gate.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="gate" tilewidth="34" tileheight="52" tilecount="5" columns="5" objectalignment="bottom">
+<tileset version="1.10" tiledversion="1.12.2" name="gate" tilewidth="34" tileheight="52" tilecount="10" columns="5" objectalignment="bottom">
  <grid orientation="isometric" width="64" height="32"/>
  <properties>
   <property name="Solid" type="bool" value="true"/>
  </properties>
- <image source="gate.png" width="170" height="52"/>
+ <image source="gate.png" width="170" height="104"/>
  <tile id="0">
   <objectgroup draworder="index" id="3">
    <object id="3" x="7" y="22.5" width="33" height="32"/>
@@ -14,6 +14,14 @@
    <frame tileid="1" duration="50"/>
    <frame tileid="2" duration="50"/>
    <frame tileid="3" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="6" duration="100"/>
   </animation>
  </tile>
 </tileset>
